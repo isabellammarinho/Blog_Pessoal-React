4bde5440565a3c8fb2a63dadecdf720eac44e125
Isabella Marinho <isabellamarcelomarinho@gmail.com>
2021-12-01 20:37:34 -0300

    Implementando rotas

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,18 +1,45 @@
+import { Box } from '@mui/system';
 import React from 'react';
-import './Home.css';
-/* Criando componente */
+import Typography from '@mui/material/Typography';
+import { Grid, Button } from '@mui/material';
+import "./Home.css";
 
-/* (tirando o div e utilizando somente as tags de abertura e fechamento) */
-/* fragment" forma abreviada para o react retornar uma lista de elemntos; */
+function Home() {
 
-function Home(){
-    return(
-        <> 
-            <h1 className="titulo">Home</h1>
-            <img src="https://i.imgur.com/uDVZ85G.jpeg" alt="Imagem Tela Inicial" className="img"/>
-        </>
-    );
 
+    return (
+        <Grid container className="background">
+            <Grid item xs={12}>
+
+                <Box display="flex" justifyContent="center" alignItems="center" height="80vh" >
+                    <Box className="card" width={270} height="50vh" borderRadius={5} marginTop={12} display="center" justifyContent="center" alignItems="center">
+                        <Box >
+                            <Typography variant="h4" color="white" align="center">
+                                Login
+                            </Typography>
+
+                            <form >
+                                <Box marginY={4}>
+                                    <input type="text" className="color-input"
+                                        placeholder="Email" />
+                                </Box>
+
+                                <Box marginY={4}>
+
+                                    <input type="password" className="color-input"
+                                        placeholder="Senha" />
+                                </Box>
+                                <Button variant="contained" color="primary" className="botao">
+                                    Login
+                                </Button>
+                            </form>
+                        </Box>
+                    </Box>
+                </Box>
+            </Grid>
+
+        </Grid>
+    )
 }
 
-export default Home; 
+export default Home
